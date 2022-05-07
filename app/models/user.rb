e319c3f246b57c5071a6e605f7c4588bb45c337e
WilfriedPaillot <paillotwilfried@gmail.com>
2022-05-07 02:30:26 +0200

    Add roles & after_init callback to set it + relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+
+  enum role: [:student, :instructor, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  # Associations for instructor
+  has_many :trainings, dependent: :destroy
+  has_many :sections, through: :trainings
+  has_many :lessons, through: :sections
+  # Associations for student
+  has_many :trainings_users, dependent: :destroy
+  has_many :trainings, through: :trainings_users
+  has_many :sections, through: :trainings
+  has_many :lessons, through: :sections
+
+  private
+    def set_default_role
+      self.role ||= :student
+    end
 end
+  
+  # # Associations for instructor
+  # has_many :trainings, dependent: :destroy
+  # has_many :sections, through: :trainings
+  # has_many :lessons, through: :sections
+  # # Associations for student
+  # has_many :trainings_users, dependent: :destroy
+  # has_many :following_trainings, through: :trainings_users, source: :training
