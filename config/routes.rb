af4b78e7f40a71cfd2879d9b24b5771653466821
WilfriedPaillot <paillotwilfried@gmail.com>
2022-05-07 19:53:50 +0200

    Update homepage w/ link_to trainings#show & routes for nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   root to: 'static_pages#homepage'
     get 'static_pages/contact'
     get 'static_pages/about'
+
+  resources :trainings do
+    resources :sections do
+      resources :lessons
+    end
+  end
+
+  
 end
