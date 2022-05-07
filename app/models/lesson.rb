8b38660a94c94c2d35d75fefa3f08df87dc8a498
WilfriedPaillot <paillotwilfried@gmail.com>
2022-05-07 02:38:25 +0200

    Update models w/ relations + validations

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,13 @@
 class Lesson < ApplicationRecord
   belongs_to :section
-  has_
+  
+  # Validates presence of minimum data for a lesson
+  validates :title, :content, :section_id, presence: { message: "doit être renseigné" }
+  # Validates title length and uniqueness within a section
+  validates :title, uniqueness: { scope: :section_id, message: ' est déjà utilisé pour une leçon de cette section' }
+  validates :title, length: { in: 10..100, message: ' doit contenir entre 10 et 100 caractères' }, on: :create
+  
+  # Validates content length
+  validates :content, length: { minimum: 150, message: ' doit contenir au moins 150 caractères' }, on: :create
+
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -1,4 +1,14 @@
 class Training < ApplicationRecord
   belongs_to :user
   has_many :sections, dependent: :destroy
+  has_many :lessons, through: :sections
+  has_many :trainings_users
+
+  # Validates presence of minimum data for a training
+  validates :title, :description, :user_id, presence: { message: "doit être renseigné" }
+  # Validates title length and uniqueness for user (insctructor)
+  validates :title, uniqueness: { scope: :user_id, message: ' est déjà utilisé pour l\'un de vos cours' }
+  validates :title, length: { in: 10..50, message: ' doit contenir entre 10 et 50 caractères' }, on: :create
+  # Validates description length
+  validates :description, length: { minimum: 50, message: ' doit contenir au moins 50 caractères' }, on: :create
 end
