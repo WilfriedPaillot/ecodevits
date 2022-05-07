a62564ae125f81b619a4b0af291b0c403181d15a
WilfriedPaillot <paillotwilfried@gmail.com>
2022-05-07 02:51:56 +0200

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,16 +17,15 @@ class User < ApplicationRecord
   has_many :sections, through: :trainings
   has_many :lessons, through: :sections
 
+  validates :username, presence: true, uniqueness: true
+  validates :username, 
+    length: { in: 3..15, message: ' doit contenir entre 3 et 15 caractères' },
+    format: { with: /\A[a-zA-Z0-9_]+\z/, message: ' doit contenir uniquement des lettres, des chiffres et des underscores' }
+    on: :create
+
   private
     def set_default_role
       self.role ||= :student
     end
-end
-  
-  # # Associations for instructor
-  # has_many :trainings, dependent: :destroy
-  # has_many :sections, through: :trainings
-  # has_many :lessons, through: :sections
-  # # Associations for student
-  # has_many :trainings_users, dependent: :destroy
-  # has_many :following_trainings, through: :trainings_users, source: :training
+  end
+
