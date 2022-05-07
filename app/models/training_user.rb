c61bc6a8c887090081d2e4664cf751241c8c44fe
WilfriedPaillot <paillotwilfried@gmail.com>
2022-05-07 02:55:28 +0200

    Update training_user model w/ validations

--- app/models/training_user.rb
+++ app/models/training_user.rb
@@ -1,4 +1,7 @@
 class TrainingUser < ApplicationRecord
   belongs_to :training
   belongs_to :user
+
+  validates :training_id, :user_id, presence: true
+  validates :training_id, uniqueness: { scope: :user_id, message: ' est déjà inscrit à ce cours' }
 end
