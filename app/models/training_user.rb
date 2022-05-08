6d0cfb8d0200580b6713b101d8024e7f072fa350
WilfriedPaillot <paillotwilfried@gmail.com>
2022-05-08 02:50:46 +0200

    Update routes w/ trainings & dashboard scope & get

--- app/models/training_user.rb
+++
@@ -1,8 +0,0 @@
-class TrainingUser < ApplicationRecord
-  belongs_to :training
-  belongs_to :user
-
-  validates :training_id, :user_id, presence: true
-  validates :training_id, uniqueness: { scope: :user_id, message: ' est déjà inscrit à ce cours' }
-  validates :completion_rate, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100, message: ' doit être un nombre entier compris entre 0 et 100' }
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'static_pages#homepage'
-    get 'static_pages/contact'
-    get 'static_pages/about'
-
-  resources :trainings do
-    resources :sections do
-      resources :lessons
+  get 'static_pages/contact'
+  get 'static_pages/about'
+  
+  resources :trainings
+  resources :trainings_users, only: [:new, :create]
+  
+  scope '/dashboard' do
+    resources :trainings do
+      resources :sections do
+        resources :lessons
+      end
     end
   end
+
+  get 'dashboard/index'
+  get 'dashboard/student', to: 'dashboard#student'
+  get 'dashboard/instructor' , to: 'dashboard#instructor'
+  get 'dashboard/admin' , to: 'dashboard#admin'
 
   
 end
