dd664e69db8baf1b878295f87100aa7fd0fbd3bd
WilfriedPaillot <paillotwilfried@gmail.com>
2022-05-08 16:30:16 +0200

    Update lesson model w/ method

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -10,4 +10,22 @@ class Lesson < ApplicationRecord
   # Validates content length
   validates :content, length: { minimum: 150, message: ' doit contenir au moins 150 caractères' }, on: :create
 
+
+  # Returns the next lesson in the section
+  def next
+    section.lessons.find_by(id: self.id + 1) if self.id != section.lessons.last.id
+  end
+  # Returns the first lesson of the next section
+  def first_of_next_section
+    section.training.sections.find_by(id: self.section.id + 1).lessons.first if self.section.id != section.training.sections.last.id
+  end
+  # Returns true if the lesson isnt the last one in the section
+  def isnt_last_lesson?
+    self.id != section.lessons.last.id
+  end
+  # Returns true if the lesson's training count more than one section AND the section isnt the last one
+  def next_section?
+    self.section.training.sections.count > 1 && self.section.id != section.training.sections.last.id
+  end
+
 end
