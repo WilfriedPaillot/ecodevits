c1c241c47e8873f6ce0d9e73fd24e83b81e64a79
WilfriedPaillot <paillotwilfried@gmail.com>
2022-05-14 01:06:13 +0200

    Update app controller for protected method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   add_flash_types :danger, :info, :warning, :success, :messages
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
   # Devise overrides
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
