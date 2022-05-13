4db357dadf3569106dc6a9581ce95f682a7c61a8
WilfriedPaillot <paillotwilfried@gmail.com>
2022-05-13 15:26:05 +0200

    Update User model w/ validations for common fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,22 @@ class User < ApplicationRecord
     length: { in: 3..15, message: ' doit contenir entre 3 et 15 caractères' },
     format: { with: /\A[a-zA-Z0-9_]+\z/, message: ' doit contenir uniquement des lettres, des chiffres et des underscores' },
     uniqueness: { case_sensitive: false }, on: :create
-
+    validates :first_name, :last_name, allow_blank: true, 
+    length: { minimum: 2, maximum: 20, message: "doit être compris entre 2 et 20 caractères" }, 
+    format: { with: /\A[a-zA-Z\']+\z/ , message: "ne doit contenir que des lettres" },
+    on: :update
+  validates :adress, allow_blank: true, 
+    length: { maximum: 100, message: "doit être inférieur à 100 caractères" }, 
+    on: :update
+  validates :zipcode, allow_blank: true, 
+    length: { is: 5, message: "doit être de 5 caractères"}, 
+    format: { with: /\A(([0-8][1-9])|(9[0-5]))[0-9]{3}|((97[1-8])[0-9]{2})|((98[1-8])[0-9]{2})|99138\z/ , message: "inexistant" }, 
+    on: :update
+  validates :city, allow_blank: true, 
+    length: { maximum: 25, message: "doit être inférieur à 25 caractères" }, 
+    format: { with: /\A[a-zA-Z\-]+\z/ , message: "ne doit contenir que des lettres" }, 
+    on: :update
+    
   private
     def set_default_role
       self.role ||= :student
