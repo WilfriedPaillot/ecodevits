e61cc7e83ab093bfebb1961e6bd1bb469651b7ab
WilfriedPaillot <paillotwilfried@gmail.com>
2022-05-19 02:32:35 +0200

    Update cloudinary config for initializer cloudinary.rb + delete application.yml config file

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,8 @@
+Cloudinary.config do |config|
+  config.cloud_name: ENV['CLOUDINARY_CLOUD_NAME']
+  config.api_key: ENV['CLOUDINARY_API_KEY']
+  config.api_secret: ENV['CLOUDINARY_SECRET_ACCESS_KEY']
+  config.secure: true
+  config.enhance_image_tag: true
+  config.static_file_support: true
+end
