fb2ee445f9994bd4f30086d873e340d1cd6608b7
WilfriedPaillot <paillotwilfried@gmail.com>
2022-05-31 03:03:05 +0200

    Update assets initializers & update js assets  + create signUp.js script + add somes images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require bootstrap_scripts
+//= require signUp
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.paths << Rails.root.join("lib", "assets", "stylesheets")
+Rails.application.config.assets.paths << Rails.root.join("lib", "assets", "javascripts")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
 
