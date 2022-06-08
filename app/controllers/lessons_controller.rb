f760e8b1b3735f6343aef8e841a458ff87974614
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-08 02:37:39 +0200

    Update Lessons controller w/ callback & new create methods + prvate set_lesson & can_edit?

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,8 @@
 class LessonsController < ApplicationController
+  before_action :set_lesson, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :can_edit_lesson?, only: [:new, :edit, :update, :destroy]
+
   def index
   end
 
@@ -9,6 +13,9 @@ class LessonsController < ApplicationController
   end
 
   def new
+    @lesson = Lesson.new
+    @section = Section.find(params[:section_id])
+    @training = @section.training
   end
 
   def edit
@@ -18,6 +25,14 @@ class LessonsController < ApplicationController
   end
 
   def create
+    @lesson = Lesson.new(lesson_params).tap do |lesson|
+      lesson.section_id = params[:section_id]
+    end
+    if @lesson.save
+      redirect_to training_path(@lesson.section.training)
+    else
+      redirect_to @lesson, notice: 'Leçon non créée.'
+    end
   end
 
   def destroy
@@ -28,4 +43,13 @@ class LessonsController < ApplicationController
       params.require(:lesson).permit(:title, :content, :section_id, :lessons_completed, :completed, :video, :url, :thumbnail, documents: [])
     end
 
+    def set_lesson
+      @lesson = Lesson.find(params[:id])
+    end
+
+    def can_edit_lesson?
+      unless current_user.admin? || current_user.id == (Training.find(params[:training_id]).user_id) 
+        redirect_to trainings_url, notice: 'Bien essayé, mais vous n\'avez pas accès à cette page.'
+      end
+    end
 end
